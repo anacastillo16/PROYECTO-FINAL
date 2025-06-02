e23b766a17abfc27e4a50013eb5c0845ae0269ce
Ana Castillo Ramírez <anacastilloramirez16@gmail.com>
2025-06-02 21:00:50 +0200

    :goal_net: rutas controladas

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Trabajador\BookController;
 use App\Http\Controllers\Trabajador\AuthorController;
 use App\Http\Controllers\Trabajador\EditorialController;
 use App\Http\Controllers\Usuario\UserController;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\UserMiddleware;
 use Illuminate\Support\Facades\Route;
 
 // RUTAS PÚBLICAS
@@ -25,42 +27,47 @@ Route::middleware(['auth'])->group(function () {
     //CERRAR SESIÓN
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-    //INDEX TRABAJADOR
-    Route::get('/indexTRABAJADOR', [BookController::class, 'index'])->name('index.trabajador');
+    //TRABAJADOR
+    Route::middleware([AdminMiddleware::class])->group(function () {
+        //INDEX 
+        Route::get('/indexTRABAJADOR', [BookController::class, 'index'])->name('index.trabajador');
 
-    //INDEX USUARIO
-    Route::get('/indexUSUARIO', [UserController::class, 'index'])->name('index.usuario');
+        //BOOKS
+        Route::get('trabajador/booksDetails/{id}', [BookController::class, 'show'])->name('trabajador.books.show');
+        Route::put('trabajador/editarLibro/{id}', [BookController::class, 'update'])->name('trabajador.books.update');
+        Route::post('trabajador/libros', [BookController::class, 'store'])->name('trabajador.books.store');
+        Route::delete('trabajador/booksDetails/{id}', [BookController::class, 'destroy'])->name('trabajador.books.destroy');
+        
+        //AUTORS
+        Route::get('trabajador/autores', [AuthorController::class, 'index'])->name('trabajador.autors.index');
+        Route::get('trabajador/autorDetails/{id}', [AuthorController::class, 'show'])->name('trabajador.autors.show');
+        Route::put('trabajador/editarAutor/{id}', [AuthorController::class, 'update'])->name('trabajador.autors.update');
+        Route::post('trabajador/autores', [AuthorController::class, 'store'])->name('trabajador.autors.store');
+        Route::delete('trabajador/verAutores/{id}', [AuthorController::class, 'destroy'])->name('trabajador.autors.destroy');
 
-    //BOOKS
-    //TRABAJADOR BOOKS
-    Route::get('trabajador/booksDetails/{id}', [BookController::class, 'show'])->name('trabajador.books.show');
-    Route::put('trabajador/editarLibro/{id}', [BookController::class, 'update'])->name('trabajador.books.update');
-    Route::post('trabajador/libros', [BookController::class, 'store'])->name('trabajador.books.store');
-    Route::delete('trabajador/booksDetails/{id}', [BookController::class, 'destroy'])->name('trabajador.books.destroy');
+        //EDITORIALS
+        Route::get('trabajador/editoriales', [EditorialController::class, 'index'])->name('trabajador.editorials.index');
+        Route::get('trabajador/editorialDetails/{id}', [EditorialController::class, 'show'])->name('trabajador.editorials.show');
+        Route::put('trabajador/editarEditorial/{id}', [EditorialController::class, 'update'])->name('trabajador.editorials.update');
+        Route::post('trabajador/editoriales', [EditorialController::class, 'store'])->name('trabajador.editorials.store');
+        Route::delete('trabajador/verEditorials/{id}', [EditorialController::class, 'destroy'])->name( 'trabajador.editorials.destroy');
 
-    //USUARIO BOOKS
-    Route::get('usuario/booksDetails/{id}', [UserController::class, 'showBook'])->name('usuario.books.show');
+    });
 
-    //AUTHORS
-    //TRABAJADOR AUTHORS
-    Route::get('trabajador/autores', [AuthorController::class, 'index'])->name('trabajador.autors.index');
-    Route::get('trabajador/autorDetails/{id}', [AuthorController::class, 'show'])->name('trabajador.autors.show');
-    Route::put('trabajador/editarAutor/{id}', [AuthorController::class, 'update'])->name('trabajador.autors.update');
-    Route::post('trabajador/autores', [AuthorController::class, 'store'])->name('trabajador.autors.store');
-    Route::delete('trabajador/verAutores/{id}', [AuthorController::class, 'destroy'])->name('trabajador.autors.destroy');
+    //USUARIO
+    Route::middleware([UserMiddleware::class])->group(function () {
+        //INDEX 
+        Route::get('/indexUSUARIO', [UserController::class, 'index'])->name('index.usuario');
 
-    //USUARIO AUTHORS
-    Route::get('usuario/autores', [UserController::class, 'showAutors'])->name('usuario.autors.index');
-    Route::get('usuario/autorDetails/{id}', [UserController::class, 'showAutor'])->name('usuario.autors.show');
+        //BOOKS
+        Route::get('usuario/booksDetails/{id}', [UserController::class, 'showBook'])->name('usuario.books.show');
 
-    //EDITORIALS
-    Route::get('trabajador/editoriales', [EditorialController::class, 'index'])->name('trabajador.editorials.index');
-    Route::get('trabajador/editorialDetails/{id}', [EditorialController::class, 'show'])->name('trabajador.editorials.show');
-    Route::put('trabajador/editarEditorial/{id}', [EditorialController::class, 'update'])->name('trabajador.editorials.update');
-    Route::post('trabajador/editoriales', [EditorialController::class, 'store'])->name('trabajador.editorials.store');
-    Route::delete('trabajador/verEditorials/{id}', [EditorialController::class, 'destroy'])->name( 'trabajador.editorials.destroy');
+        //AUTHORS
+        Route::get('usuario/autores', [UserController::class, 'showAutors'])->name('usuario.autors.index');
+        Route::get('usuario/autorDetails/{id}', [UserController::class, 'showAutor'])->name('usuario.autors.show');
 
-    //USUARIO EDITORIALS
-    Route::get('usuario/editoriales', [UserController::class, 'showEditorials'])->name('usuario.editorials.index');
-    Route::get('usuario/editorialDetails/{id}', [UserController::class, 'showEditorial'])->name('usuario.editorials.show');
+        //EDITORIALS
+        Route::get('usuario/editoriales', [UserController::class, 'showEditorials'])->name('usuario.editorials.index');
+        Route::get('usuario/editorialDetails/{id}', [UserController::class, 'showEditorial'])->name('usuario.editorials.show');
+    });
 });
