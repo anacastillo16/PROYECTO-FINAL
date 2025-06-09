b933440bae80d15e0dc5c4da7e018c3c55cf522f
Ana Castillo Ramírez <anacastilloramirez16@gmail.com>
2025-06-09 19:33:52 +0200

    :art: cambiando paleta de colores cabecera usuario

--- resources/views/layouts/usuario/header.blade.php
+++ resources/views/layouts/usuario/header.blade.php
@@ -11,26 +11,26 @@
         <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarTrabajador">
             <ul class="navbar-nav me-auto mb-0 d-flex flex-row gap-3">
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="{{ route('usuario.autors.index') }}">Autores</a>
+                    <a class="nav-link text-white button-primary-custom" href="{{ route('usuario.autors.index') }}">Autores</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="{{ route('usuario.editorials.index') }}">Editoriales</a>
+                    <a class="nav-link text-white button-primary-custom" href="{{ route('usuario.editorials.index') }}">Editoriales</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="{{ route('favoritos.index') }}">Favoritos</a>
+                    <a class="nav-link text-white button-primary-custom" href="{{ route('favoritos.index') }}">Favoritos</a>
                 </li>
             </ul>
 
             <ul class="navbar-nav ms-auto mb-0 d-flex flex-row align-items-center gap-3">
                 <li class="nav-item">
-                    <a href="{{ route('perfil.edit') }}" class="navbar-text text-white fw-semibold">
+                    <a href="{{ route('perfil.edit') }}" class="navbar-text text-white fw-semibold button-primary-custom">
                         {{ Auth::user()->name }}
                     </a>
                 </li>
                 <li class="nav-item">
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <button type="submit" class="btn button-secondary btn-sm">Cerrar sesión</button>
+                        <button type="submit" class="button-primary-custom">Cerrar sesión</button>
                     </form>
                 </li>
             </ul>
