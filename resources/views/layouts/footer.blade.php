1d8f0308a25560ac7c4f9b6ca01cad7f55eb4a79
anacastillo16 <72932228+anacastillo16@users.noreply.github.com>
2025-06-09 08:45:57 +0200

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-primary text-white py-3 mt-auto">
+<footer style="background-color: #6CBF84;" class="text-white py-3 mt-auto">
     <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
         <div>
             &copy; Ana {{ date('Y') }}. Todos los derechos reservados.
