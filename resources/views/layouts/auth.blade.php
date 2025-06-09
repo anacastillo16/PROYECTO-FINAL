b0197c05e6d5bc3d20a9613b230a1a8173993ef7
anacastillo16 <72932228+anacastillo16@users.noreply.github.com>
2025-06-09 08:49:56 +0200

    Update auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>@yield('title')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+    @vite(['resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 <body class="bg-light d-flex justify-content-center align-items-center vh-100">
