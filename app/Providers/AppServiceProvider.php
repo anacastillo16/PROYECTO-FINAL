85c2e9d044690bbb40c3bf78030d8f3aa8d25502
anacastillo16 <72932228+anacastillo16@users.noreply.github.com>
2025-06-10 07:51:42 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,13 +21,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-        
-        if (config('database.default') === 'sqlite') {
-            $databasePath = database_path('database.sqlite');
-            if (!file_exists($databasePath)) {
-                File::put($databasePath, '');
-            }
-            DB::statement("PRAGMA foreign_keys=ON");
-        }
     }
 }
