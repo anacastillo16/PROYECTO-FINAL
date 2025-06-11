9f02ece4780cd6d4184e5b401c1385940914904f
anacastillo16 <72932228+anacastillo16@users.noreply.github.com>
2025-06-11 10:57:49 +0200

    :zap: improve UI experience

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'TFG')</title>
 
+    <script>
+        window.hasFormErrors = {{ $errors->any() ? 'true' : 'false' }};
+    </script>
     @vite(['resources/js/app.js'])
     @vite(['resources/css/app.css'])
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
