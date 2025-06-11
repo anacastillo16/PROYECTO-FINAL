2bf23615d97f78424d115aefdb3abc87ee7c7dcb
anacastillo16 <72932228+anacastillo16@users.noreply.github.com>
2025-06-11 10:49:33 +0200

    :zap: improve UI experience

--- resources/views/perfil/actualizarPerfil.blade.php
+++ resources/views/perfil/actualizarPerfil.blade.php
@@ -138,25 +138,3 @@
     </div>
 </main>
 @endsection
-
-@push('scripts')
-<script>
-    document.querySelectorAll('.toggle-password').forEach(button => {
-        button.addEventListener('click', function () {
-            const targetId = this.getAttribute('data-target');
-            const input = document.getElementById(targetId);
-            const icon = this.querySelector('i');
-
-            if (input.type === 'password') {
-                input.type = 'text';
-                icon.classList.remove('bi-eye');
-                icon.classList.add('bi-eye-slash');
-            } else {
-                input.type = 'password';
-                icon.classList.remove('bi-eye-slash');
-                icon.classList.add('bi-eye');
-            }
-        });
-    });
-</script>
-@endpush
