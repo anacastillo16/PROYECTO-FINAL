94d31dad0182d58b3d06aec5400007af894b8451
anacastillo16 <72932228+anacastillo16@users.noreply.github.com>
2025-06-11 10:44:54 +0200

    :zap: improve UI experience

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('title')</title>
     
     @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 <body class="bg-light d-flex justify-content-center align-items-center vh-100">
