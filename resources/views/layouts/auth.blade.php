3e0f1345dea357a9a791c2a7499a1f068074f79f
anacastillo16 <72932228+anacastillo16@users.noreply.github.com>
2025-06-11 10:46:56 +0200

    :zap: improve UI experience

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -14,26 +14,5 @@
     <div style="width: 380px; border-radius: 12px;">
         @yield('content')
     </div>
-    
-    <script>
-        document.querySelectorAll('.toggle-password').forEach(button => {
-            button.addEventListener('click', () => {
-                const inputId = button.getAttribute('data-target');
-                const input = document.getElementById(inputId);
-                const icon = button.querySelector('i');
-
-                if (input.type === 'password') {
-                    input.type = 'text';
-                    icon.classList.remove('bi-eye');
-                    icon.classList.add('bi-eye-slash');
-                } else {
-                    input.type = 'password';
-                    icon.classList.remove('bi-eye-slash');
-                    icon.classList.add('bi-eye');
-                }
-            });
-        });
-    </script>
-
 </body>
 </html>
