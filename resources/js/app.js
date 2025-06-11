ff4ba7d3a782437d389c0d48fd12b309d5b271bc
anacastillo16 <72932228+anacastillo16@users.noreply.github.com>
2025-06-11 10:47:51 +0200

    :zap: improve UI experience

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,22 @@ import 'bootstrap';  // Solo JS de Bootstrap
 import * as bootstrap from 'bootstrap';
 
 window.bootstrap = bootstrap;
+
+
+document.querySelectorAll('.toggle-password').forEach(button => {
+    button.addEventListener('click', () => {
+        const inputId = button.getAttribute('data-target');
+        const input = document.getElementById(inputId);
+        const icon = button.querySelector('i');
+
+        if (input.type === 'password') {
+            input.type = 'text';
+            icon.classList.remove('bi-eye');
+            icon.classList.add('bi-eye-slash');
+        } else {
+            input.type = 'password';
+            icon.classList.remove('bi-eye-slash');
+            icon.classList.add('bi-eye');
+        }
+    });
+});
